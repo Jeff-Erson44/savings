c1c3d0ffca6e9bc65c197d47579e306ee9bb1542
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2024-08-24 01:05:35 +0200

    push pwa

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Lien vers le manifest.json */}
+        <link rel="manifest" href="/manifest.json"/>
+        
+        {/* Icône pour la PWA */}
+        <link rel="icon" href="/icons/icon.png" />
+
+        {/* Couleur du thème pour la barre d'adresse sur mobile */}
+        <meta name="theme-color" content="#317EFB" />
+
+        {/* Autres balises meta ou links peuvent être ajoutés ici */}
+      </Head>
       <body>
         <Main />
         <NextScript />
